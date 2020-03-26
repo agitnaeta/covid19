869f8b6ecea6e606efcf8e8047a79b47f5e03f57
Agit Naeta <ana@voxteneo.asia>
2020-03-26 20:46:34 +0700

    Corona Init Flile

---
+++ app.js
@@ -0,0 +1,136 @@
+const OUR_COUNTRY = 'indonesia';
+const DEFAULT_LOCATION = {
+    lat :-6.8586102 ,
+    lng : 107.9163895
+}
+$(document).ready(function(){
+    $.get('https://covid19-public.digitalservice.id/analytics/longlat/',function(response){
+        navigator.geolocation.getCurrentPosition((location)=>{
+            let our_location = {
+                lat : location.coords.latitude,
+                lng : location.coords.longitude
+            }
+            $('#last_update').html(response.last_update)
+            localStorage.setItem('data',JSON.stringify(response))
+            autodetect(our_location,response.data,'Kasus terdekat berdasar auto detect')
+            map(response.data,our_location)
+        },()=>{
+            alert('Please reload')
+        },{enableHighAccuracy:true});
+
+    })
+})
+
+function map(data,loc){
+    let mainMap = L.map('mapid').setView([loc.lat,loc.lng], 15);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mainMap);
+
+    createMarker(data,mainMap)
+	mainMap.on('click', detect);
+    return mainMap
+}
+
+function createMarker(data,mainMap){
+    data.map((value,index)=>{   
+       if(value.alamat_longitude!=null){
+        L.marker([
+            value.alamat_latitude,
+            value.alamat_longitude
+        ],{
+            icon : detectStatus(value.status)
+        })
+        .addTo(mainMap);
+       }
+    })
+}
+
+function detectStatus(status){
+    let color = {
+        ODP: blueIcon,
+        PDP: yellowIcon,
+        Positif: redIcon,
+    }
+    return color[status]
+}
+
+function findLocation(){
+    let city = $("#location").val()
+    if(city.length > 3){
+        $.get('https://nominatim.openstreetmap.org/search?city='+city+'&country=indonesia&format=json',
+        async function(data){
+           // get the first record 
+           if(data.length>0 && data[0]!==undefined){
+               let location_search =  {
+                    lat : data[0].lat,
+                    lng : data[0].lon,
+                }
+                let jsonData = JSON.parse(localStorage.getItem('data')).data
+
+                await $('.canva').html('')
+                await $('.canva').html(`<div id="mapid" style="width:100%; height: 700px;"></div>`)
+                await map(jsonData,location_search)
+                await autodetect(location_search,jsonData,'Kasus terdekat bedasar pusat kota')
+               
+           }
+        })
+    }
+    else{
+        alert('Lokasi tidak valid')
+    }
+}
+function detect(e){    
+    let jsonData = JSON.parse(localStorage.getItem('data')).data
+    autodetect(e.latlng,jsonData,'Kasus terdekat berdasar lokasi yang anda Klik')
+}
+
+function autodetect(location,data,text=''){
+    let arr  = data.map((value)=>{
+       let distance = getDistanceBetweenPoints(location,{
+        lat : value.alamat_latitude,
+        lng : value.alamat_longitude
+       })
+       return (distance / 1000)
+    })
+    $('#your_location').html(`<h3> ${text}</h3><h1>${arr.sort()[0].toFixed(2)} Km</h1>`)
+}
+
+/**
+ * Converts degrees to radians.
+ * 
+ * @param degrees Number of degrees.
+ */
+function degreesToRadians(degrees){
+    return degrees * Math.PI / 180;
+}
+
+
+function getDistanceBetweenPoints(center, target){
+// The radius of the planet earth in meters
+    let R = 6378137;
+    let dLat = degreesToRadians(target.lat - center.lat);
+    let dLong = degreesToRadians(target.lng - center.lng);
+    let a = Math.sin(dLat / 2)
+            *
+            Math.sin(dLat / 2) 
+            +
+            Math.cos(degreesToRadians(center.lat)) 
+            * 
+            Math.cos(degreesToRadians(center.lat)) 
+            *
+            Math.sin(dLong / 2) 
+            * 
+            Math.sin(dLong / 2);
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let distance = R * c;
+    return distance;
+}
+  
---
+++ icon.js
@@ -0,0 +1,71 @@
+var blueIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-blue.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var goldIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-gold.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-red.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-green.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var orangeIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-orange.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var yellowIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-orange.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var violetIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-violet.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greyIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-grey.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var blackIcon = new L.Icon({
+	iconUrl: 'icon/marker-icon-2x-black.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
